2b89106cdbd9e6906e65ad4ce7fea252db19941c
jiangfan <jf1132694551@163.com>
2019-03-04 13:32:29 +0800

    sadds

--- core/indexer_store.go
+++ core/indexer_store.go
@@ -82,7 +82,7 @@ func (indexer *Indexer)StoreRecoverForwardIndex(dbPath string,StoreEngine string
 		//正向索引结构
 		var fowardindex StoreForwardIndex
 		err := dec.Decode(&fowardindex)
-		log.Println(fowardindex)
+		log.Println("forwardindex:",fowardindex)
 		if err == nil {
 			docsState[keystring] = 0
 			docTokenLens[keystring] = fowardindex.tokenLen
