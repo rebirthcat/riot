9f45ec262eef3719ead6783da0ed71511bfded07
jiangfan <jf1132694551@163.com>
2019-05-28 17:00:51 +0800

    ok

--- core/indexer.go
+++ core/indexer.go
@@ -468,7 +468,7 @@ func (indexer *Indexer) RemoveDocs(docs *types.DocsId) {
 		docsPointer := sort.Search(
 			len(*docs),
 			func(i int) bool {
-				if docs != nil || len(*docs) > 0 {
+				if docs != nil && *docs!=nil && len(*docs) > 0 {
 					return (*docs)[i] >= indices.docIds[0]
 				}else {
 					return false
